fff4247902693bf4b35d56e3a73acaab8af156c2
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-27 21:09:20 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post
+    attr_accessor :title, :author
+    def initialize
+        @title = "Hello World"
+        # @author = self.new.author.name
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,6 @@
+class Song
+    attr_accessor :title, :artist
+    def initialize
+        @title = "7/11"
+    end
+end
